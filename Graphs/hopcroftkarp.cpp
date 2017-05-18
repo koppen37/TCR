cdb5518cdeb4dd1bf118618ca22a3ed556043525
HWMdeJong <hieronymusdejong@gmail.com>
2017-05-18 11:37:32 +0200

    Switch to int64_t
    Hi Jul

--- Graphs/hopcroftkarp.cpp
+++ Graphs/hopcroftkarp.cpp
@@ -1,9 +1,8 @@
-#define INF (1<<30)
+#define INF (1LL << 60)
 
-//the code is 1 indexed, match should be initialized 0, id is only relevant for sort and interpreting answer.
 struct Node {
-    int match, deg, id;
-    set<int> adj;
+    int64_t match, deg, id;
+    set<int64_t> adj;
 
     bool operator<(const Node& rhs) const{
         if(deg == rhs.deg) return id > rhs.id;
@@ -12,11 +11,11 @@ struct Node {
 };
 
 vector<Node> U, V;
-vector<int> dist;
+vector<int64_t> dist;
 
 bool bfs() {
-    queue<int> Q;
-    for(int i = 1; i < U.size(); i++) {
+    queue<int64_t> Q;
+    for(int64_t i = 1; i < U.size(); i++) {
         if(U[i].match == 0) {
             dist[i] = 0;
             Q.push(i);
@@ -26,10 +25,10 @@ bool bfs() {
     }
     dist[0] = INF;
     while(!Q.empty()) {
-        int i = Q.front();
+        int64_t i = Q.front();
         Q.pop();
         if(dist[i] < dist[0]) {
-            for(const int &j : U[i].adj) {
+            for(const int64_t &j : U[i].adj) {
                 if(dist[V[j].match] == INF) {
                     dist[V[j].match] = dist[i] + 1;
                     Q.push(V[j].match);
@@ -40,9 +39,9 @@ bool bfs() {
     return dist[0] != INF;
 }
 
-bool dfs(int i) {
+bool dfs(int64_t i) {
     if(i != 0) {
-        for(const int &j : U[i].adj) {
+        for(const int64_t &j : U[i].adj) {
             if(dist[V[j].match] == dist[i] + 1) {
                 if(dfs(V[j].match)) {
                     V[j].match = i;
@@ -57,11 +56,11 @@ bool dfs(int i) {
     return true;
 }
 
-int hopcroft_karp() {
-    int matching = 0;
+int64_t hopcroft_karp() {
+    int64_t matching = 0;
     dist.assign(U.size(), INF); //This is untested
     while(bfs()) {
-        for(int i = 1; i < U.size(); i++) {
+        for(int64_t i = 1; i < U.size(); i++) {
             if(U[i].match == 0) {
                 if(dfs(i)) {
                     matching++;
