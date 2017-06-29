ec361cc4f27af067fb959a7e1cc74d9bfb94bc8d
HWMdeJong <hieronymusdejong@gmail.com>
2017-06-29 10:15:21 +0200

    Fixed bellmanford
    Fixed syntax and correct implementation to deal with negative cycles and unreachable points.

--- Graphs/bellmanford.cpp
+++ Graphs/bellmanford.cpp
@@ -4,24 +4,35 @@ struct Edge{
     int64_t u, v, w;
 };
 
-vector<int64_t> dist;
+vector<int64_t> dist, pred;
 vector<Edge> edges;
 
-//s is the start node, n is the amount of nodes.
+//s is the start node, n is the amount of nodes. dist[u] = -INF -> path (s to u) contains negative cycle.
+//Return false if there exists a negative cycle.
 bool bellmanford(int64_t s, int64_t n){
     dist.clear();
-    dist.resize(n,INF);
+    pred.clear();
+    dist.assign(n,INF);
+    pred.assign(n,-1);
     dist[s] = 0;
 
     for(int64_t i = 0; i < n-1; i++){
         for(const Edge& e: edges){
+            if(dist[e.u] == INF || dist[e.v] <= dist[e.u] + e.w) continue;
+            pred[e.v] = e.u;
             dist[e.v] = min(dist[e.v],dist[e.u] + e.w);
         }
     }
 
-    for(const Edge& e: edge){
-        if(dist[e.v] > dist[e.u] + e.w){
-            return false;
+    bool nonegcyc = true;
+    for(int64_t i = 0; i < n-1; i++){
+        for(const Edge& e: edges){
+            if(dist[e.u] == INF) continue;
+            if(dist[e.v] > dist[e.u] + e.w){
+                dist[e.v] = -INF;
+                nonegcyc = false;
+            }
         }
     }
+    return nonegcyc;
 }
